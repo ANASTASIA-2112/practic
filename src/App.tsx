49a1bd6413877289a04d13669e075a33c3e7f005
ANASTASIA2112 <anastasia.adamovich@yandex.by>
2023-01-26 11:26:55 +0300

    commit app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+
+function App() {
+    debugger
+    return (
+        <div className="App">
+            <Todolist/>
+
+
+        </div>
+    );
+}
+
+
+function Todolist() {
+    debugger
+    return (
+        <div>Todolist</div>
+    );
+}
+
+export default App;
