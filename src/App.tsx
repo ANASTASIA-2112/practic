5122f0b3ede3d8e7c8afa6c3d769626df8863b5b
ANASTASIA2112 <anastasia.adamovich@yandex.by>
2023-01-27 16:31:59 +0300

    commit 6

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 import {TaskType, Todolist} from "./Todolist";
 
 
-function  App () {
+function App() {
 
     const tasks1 = [
-        {id : 1, title: "CSS", isDone: true },
-        {id : 2, title: "JS", isDone: true },
-        {id : 3, title: "React", isDone: false },
-        {id : 4, title: "Redux", isDone: false },
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "Redux", isDone: false},
 
     ]
 
-    const tasks2 : Array<TaskType> = [
-        {id : 1, title: "Hello world", isDone : false },
+    const tasks2: Array<TaskType> = [
+        {id: 1, title: "Hello world", isDone: false},
 
 
     ]
@@ -22,9 +22,8 @@ function  App () {
 
     return (
         <div className="App">
-            <Todolist title= "What to learn"  tasks = {tasks1}/>
-            <Todolist title="Songs" tasks = {tasks2}/>
-
+            <Todolist title="What to learn" tasks={tasks1}/>
+            <Todolist title="Songs" tasks={tasks2}/>
 
         </div>
     );
