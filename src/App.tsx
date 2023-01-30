cdf5ac9297d5bd56a69bb34d4022e3d1b0793f51
ANASTASIA2112 <anastasia.adamovich@yandex.by>
2023-01-30 09:49:49 +0300

    commit 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Header from "./site/Нeader";
-import Body from "./site/Body";
-import Footer from "./site/Footer";
+import {Todolist} from "./Todolist";
+
+function useState2(data: any) {
+    return [data, () => {
+    }];
+}
+
+let arr = useState2([{}, {}, {}]);
+
+let tasks = arr[0];
+let setTasks = arr[1];
 
 
 function App() {
+
+    let initTasks = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "Redux", isDone: false},
+
+    ]
+
+
+    let arr = useState(initTasks);
+    let tasks = arr[0];
+    let setTasks = arr[1];
+
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id !== id)
+        setTasks(filteredTasks);
+    }
+
     return (
-        <div>
-            <Header/>
-            <Body/>
-            <Footer/>
+        <div className="App">
+            <Todolist title="What to learn"
+                      tasks={tasks}
+                      removeTask={removeTask}
+            />
 
         </div>
-    )
-
+    );
 }
-
 
 export default App;
