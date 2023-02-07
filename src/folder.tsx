132eb96de90675d2db3e7ae7492a82ebbe5a9d66
ANASTASIA2112 <anastasia.adamovich@yandex.by>
2023-02-07 14:37:55 +0300

    commit 2

---
+++ src/folder.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {DataType} from "./App";
+
+
+type TaskPropsType = {
+    data: DataType
+}
+
+
+export const Tasks = (props:TaskPropsType) => {
+    return (
+        <div>
+            <h1>{props.data.title}</h1>
+            <ul>
+                {props.data.tasks.map(el => {
+                    return (
+                        <li>
+                            <span>{el.taskId}</span>
+                            <span>{el.title}</span>
+                            <span>{el.isDone}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <ul>
+                {props.data.students.map(el => {
+                    return (
+                        <li>{el}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+};
+
+
+export default Tasks;
